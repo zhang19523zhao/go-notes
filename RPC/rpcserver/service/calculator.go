de83433d27ef48e229049b5e6189120555405413
zhanghao <1952398294@qq.com>
2022-08-24 19:17:43 +0800

    指针测试

--- RPC/rpcserver/service/calculator.go
+++ RPC/rpcserver/service/calculator.go
@@ -1,11 +1,15 @@
 package service
 
-import "rpcserver/data"
+import (
+	"fmt"
+	"rpcserver/data"
+)
 
 type Calculator struct {
 }
 
 func (c *Calculator) ADD(request *data.CalculatorRequest, response *data.CalculatorResponse) error {
 	response.Result = request.Right + request.Left
+	fmt.Printf("in data result: %d ", response.Result)
 	return nil
 }
