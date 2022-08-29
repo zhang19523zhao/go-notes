e68d5d537e46a8621cd7d391d8637aa2349291b8
zhanghao <1952398294@qq.com>
2022-08-29 10:22:11 +0800

    mysql 增删改查

--- MYSQL/testdb/main.go
+++ MYSQL/testdb/main.go
@@ -14,5 +14,42 @@ func main() {
 
 	fmt.Println(db, err)
 	//测试能否连接
-	fmt.Println(db.Ping())
+	//fmt.Println(db.Ping())
+
+	//执行
+	//sql => go 字符串
+	//fmt.Println(db.Exec(`
+	//create table if not  exists  testdb(
+	//    id bigint primary key auto_increment,
+	//    name varchar(15) not null default '' comment 'testdb名字'
+	//)engine=innodb default charset utf8mb4;
+	//`))
+
+	//status := fmt.Sprintf("insert into testdb values(%d, '%s');", 1111, "bob2")
+	//status2 := "insert into testdb values(?, ?)"
+	//result2, _ := db.Exec(status2, 1113, "resutl2")
+	//fmt.Println(result2.RowsAffected())
+	//fmt.Println(status)
+	//result, _ := db.Exec(`insert into testdb values(1411, "bob")`)
+	////result, _ = db.Exec(`insert into testdb values(11, "bob")`)
+	//result, _ = db.Exec(status)
+	//fmt.Println(result.RowsAffected())
+	//fmt.Println(result.LastInsertId())
+	//读
+	//rows, _ := db.Query("select id, name from testdb;")
+
+	var (
+		id   int
+		name string
+	)
+	//for rows.Next() {
+	//	rows.Scan(&id, &name)
+	//	fmt.Println(id, name)
+	//}
+
+	//直接查询一行
+	rows := db.QueryRow("select id, name from testdb where id > ?", 10)
+	rows.Scan(&id, &name)
+	fmt.Println(id, name)
+	fmt.Println(rows.Scan(id, name))
 }
